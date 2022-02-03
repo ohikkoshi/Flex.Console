f1834cdc13dbcc3f3f8b1aa7b7f4537c630d473c
ohikkoshi <ohikkoshi@icloud.com>
2022-02-03 14:18:53 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Flex.Console
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// assembly
+			var name = Assembly.GetExecutingAssembly().GetName();
+			System.Console.WriteLine($"{name.Name} ver.{name.Version}");
+
+			// args
+			foreach (var arg in args) {
+				System.Console.WriteLine($"{arg}");
+			}
+
+			// main loop
+			var task = Task.Run(() => {
+				for (int i = 1; i <= 3; i++) {
+					System.Console.WriteLine($"Hello World!({i}/3)");
+					Task.Yield();
+					//Thread.Sleep(1000);
+				}
+			});
+
+			task.Wait();
+
+			System.Console.WriteLine($"Finish.");
+		}
+	}
+}
